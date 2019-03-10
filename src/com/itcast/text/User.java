b025ea907fc20eaf178d08d6e456177ed36f0a71
zhangsan <zhangsan@itcast.cn>
2019-03-10 20:03:05 +0800

    把修改的本地工程添加到git

--- src/com/itcast/text/User.java
+++ src/com/itcast/text/User.java
@@ -5,9 +5,16 @@ public class User {
     public static void main(String[] args) {
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("如果上天能让爱重来一次,你是我唯一我的天使");
+            System.out.println("如果上天能让爱重来一次,你是我的唯一我的天使");
         }
 
+
+    }
+
+    public void text(){
+        for (int i = 0; i < 5; i++) {
+            System.out.println("♥♥♥♥♥♥♥♥♥♥♥♥♥");
+        }
     }
 
 
